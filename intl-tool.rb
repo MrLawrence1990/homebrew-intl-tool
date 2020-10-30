7bc42b16f578a68f1068ecd87e1d16346c818aaf
MrLawrence1990 <tracy0953@qq.com>
2020-10-30 16:17:41 +0800

    Update intl-tool.rb

--- intl-tool.rb
+++ intl-tool.rb
@@ -4,7 +4,7 @@
 class IntlTool < Formula
   desc "intl-tool"
   homepage "http://wiki.intra.xiaojukeji.com/pages/viewpage.action?pageId=313614371"
-  url "http://git.xiaojukeji.com/zhaochunzheng/po-translate/raw/master/intl-tool"
+  url "http://git.xiaojukeji.com/zhaochunzheng/po-translate/raw/master/intl-tool.tgz"
   version "1.0.0"
   sha256 "a8262d4bad16939389f58cfc72146c444f75253f94fd9356427b5b08c443408f"
 
